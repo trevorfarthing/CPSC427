2ce5f51227a8b5b5a1174c4124fe1aa939114c13
trevorfarthing <tfavre11@hotmail.com>
2017-04-20 20:56:32 -0700

    Best First Search function added

--- best_first_search.py
+++ best_first_search.py
@@ -3,7 +3,8 @@
 # Team Leader: Trevor Farthing (tfarthing)
 # Developer 1: Dominic Soares
 # Developer 2: Kevin Do
-# To run: $ python3.6 best_first_search.py
+# To run:  Enter in the goal state in the best_first function call (last line of main)
+#          Then run $ python3.6 best_first_search.py
 import math
 
 def main():
@@ -17,29 +18,54 @@ def main():
              'T' : [2, 1],
              'V' : [9, 11]}
 
-    #print best_first(graph,'C')
-    print(alpha_beta('C', 3, (-1)*math.inf, math.inf, True, graph))
-    
-def best_first(graph, start):
-    return 0
+    # Will be AB-pruned graph with values(alpha/beta) added at each node
+    new_g = {}
+    print(alpha_beta('C', 3, (-1)*math.inf, math.inf, True, graph, new_g))
+    print(new_g)
+    # ENTER IN GOAL NODE AS 4TH PARAMETER
+    print(best_first(graph, new_g, 'C', 3))
+          
+def best_first(graph, new_graph, start, goal):
+    closed = []
+    opn = [start]
+    scores = {} 
+    while opn:
+        # Choose to explore node with shortest path
+        curr = min(opn, key=lambda x:new_graph[x])
+        if curr == goal:
+            return closed
+        closed.append(curr)
+        opn.remove(curr)
+
+        if curr in graph:
+            for child in graph[curr]:
+                if not child in closed and child in new_graph:
+                    opn.append(child)
+    return closed
+            
 
 # Alpha-Beta pruning function
-def alpha_beta(node, depth, alpha, beta, maxPlayer, graph):
+def alpha_beta(node, depth, alpha, beta, maxPlayer, graph, new_graph):
     if depth == 0 or node.isdigit():
+        new_graph[node] = node
         return node
     if maxPlayer:
         v = math.inf*(-1)
         for child in graph[node]:
-            v = max(v, alpha_beta(child, depth - 1, alpha, beta, False, graph))
+            v = max(v, alpha_beta(child, depth - 1, alpha, beta, False, graph, new_graph))
             alpha = max(alpha, v)
+            # set value for node in pruned graph
+            new_graph[node] = v
             if beta <= alpha:
                 break
         return v
     else:
         v = math.inf
         for child in graph[node]:
-            v = min(v, alpha_beta(child, depth - 1, alpha, beta, True, graph))
+            v = min(v, alpha_beta(child, depth - 1, alpha, beta, True, graph, new_graph))
             beta = min(beta, v)
+            # set value for node in pruned graph
+            new_graph[node] = v
             if beta <= alpha:
                 break
         return v
